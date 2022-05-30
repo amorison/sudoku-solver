974ff4e2545eff0d961ef340d38fd83a8dc0869c
Adrien Morison <adrien.morison@gmail.com>
2022-05-30 01:37:57 +0100

    TUI: extract Table's cells construction in fn

--- sudoku-tui/src/ui.rs
+++ sudoku-tui/src/ui.rs
@@ -7,30 +7,33 @@ use tui::{
 
 use crate::app_state::{App, CellValue};
 
+/// Build the cell content at a given position.
+fn cell_at(app: &App, row: usize, col: usize) -> Cell<'static> {
+    let mut style = Style::default();
+    let text = match app.value_at(row, col) {
+        CellValue::Pinned(v) => {
+            style = style.fg(Color::Green).add_modifier(Modifier::BOLD);
+            format!(" {v} ")
+        },
+        CellValue::Solution(v) => {
+            format!(" {v} ")
+        }
+        CellValue::NoSolution => {
+            " . ".to_owned()
+        }
+    };
+    if (row, col) == app.current_pos() {
+        style = style.add_modifier(Modifier::BOLD | Modifier::UNDERLINED);
+    }
+    Cell::from(text).style(style)
+}
+
 /// Define the UI for a given state of the application.
 pub fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
 	let size = f.size();
     let table = Table::new(
         (0..9).map(|ir| Row::new(
-            (0..9).map(|ic| {
-                let mut style = Style::default();
-                let text = match app.value_at(ir, ic) {
-                    CellValue::Pinned(v) => {
-                        style = style.fg(Color::Green).add_modifier(Modifier::BOLD);
-                        format!(" {v} ")
-                    },
-                    CellValue::Solution(v) => {
-                        format!(" {v} ")
-                    }
-                    CellValue::NoSolution => {
-                        " . ".to_owned()
-                    }
-                };
-                if (ir, ic) == app.current_pos() {
-                    style = style.add_modifier(Modifier::BOLD | Modifier::UNDERLINED);
-                }
-                Cell::from(text).style(style)
-            })
+            (0..9).map(|ic| cell_at(app, ir, ic))
         ))
     )
     .block(Block::default().title("Sudoku").borders(Borders::ALL))
